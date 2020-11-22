d07c5c6a6cbd04cd1d158e4e0aa7abef2bd2c577
Paxnamra <bialyfeniks4@gmail.com>
2020-11-22 02:34:35 +0100

    creational pattern: builder - version with chaining interfaces - added test for order of interfaces invocation

--- src/test/java/creational/builder_interfaces/TeaTest.java
+++ src/test/java/creational/builder_interfaces/TeaTest.java
@@ -1,10 +1,9 @@
 package creational.builder_interfaces;
 
-import creational.builder_interfaces.interfaces.PrepareTea;
-import creational.builder_interfaces.models.Herbs;
-import creational.builder_interfaces.models.Mug;
-import creational.builder_interfaces.models.Water;
+import creational.builder_interfaces.models.*;
 import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,12 +18,30 @@ class TeaTest {
             .lastStep()
             .makeTea();
 
-    @Mock
-    Tea.TeaBuilder teaBuilder = mock(Tea.TeaBuilder.class);
+    @Mock IContainer mugMock;
+    @Mock ILiquid waterMock;
+    @Mock ITeaPouch teaPouch;
+    @InjectMocks Tea.TeaBuilder teaBuilder = mock(Tea.TeaBuilder.class);
 
     @Test
     void shouldTeaBuilderCreateTeaObject() {
         when(teaBuilder.makeTea()).thenReturn(teaObject);
         assertEquals(teaObject, teaBuilder.makeTea());
+    }
+
+    @Test
+    void shouldTeaBuilderCallAllInterfacesInOrder() {
+        teaBuilder.withMugFirst(mugMock);
+        teaBuilder.withLiquid(waterMock);
+        teaBuilder.withTeaPouch(teaPouch);
+        teaBuilder.lastStep();
+        teaBuilder.makeTea();
+
+        InOrder order = inOrder(teaBuilder);
+        order.verify(teaBuilder, times(1)).withMugFirst(mugMock);
+        order.verify(teaBuilder, times(1)).withLiquid(waterMock);
+        order.verify(teaBuilder, times(1)).withTeaPouch(teaPouch);
+        order.verify(teaBuilder, times(1)).lastStep();
+        order.verify(teaBuilder, times(1)).makeTea();
     }
 }
